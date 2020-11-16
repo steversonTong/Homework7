8a270047a1a214575630b5799f04016a0234a43b
Steve Tong <stevetong53@yahoo.com>
2020-11-15 21:28:56 -0800

    questions and writetoFile function made

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,54 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        message: "Title of your application?",
+        name: "title"
+    },
 
+    {
+        type: "input",
+        message: "Brief explaination of the application",
+        name: "explain"
+    },
+    
+    {
+        type: "input",
+        message: "What is this application for?",
+        name: "what"
+    },
+
+    {
+        type: "input",
+        message: "Installation instruction",
+        name: "install"
+    },
+
+    {
+        type: "input",
+        message: "Contribution guidelines",
+        name: "guidelines"
+    },
+
+    {
+        type: "input",
+        message: "Test instructions",
+        name: "test"
+    },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Your file was successfully generated!");
+    });
 }
 
 // function to initialize program
